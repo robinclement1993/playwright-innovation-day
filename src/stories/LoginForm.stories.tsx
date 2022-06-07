63a21f29bfd4f3762f3e74cc54164f9323173691
robinclement1993 <41688771+robinclement1993@users.noreply.github.com>
2022-06-07 15:00:13 +0200

    Use ant components for looks

--- src/stories/LoginForm.stories.tsx
+++ src/stories/LoginForm.stories.tsx
@@ -3,11 +3,8 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { within, userEvent } from '@storybook/testing-library';
-
 import { expect } from '@storybook/jest';
-
 import LoginForm  from './LoginForm';
 
 export default {
@@ -29,9 +26,9 @@ FilledForm.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
   // 👇 Simulate interactions with the component
-  await userEvent.type(canvas.getByTestId('email'), 'email@provider.com');
+  await userEvent.type(canvas.getByTestId('email'), 'email@provider.com', { delay: 100 });
   
-  await userEvent.type(canvas.getByTestId('password'), 'a-random-password');
+  await userEvent.type(canvas.getByTestId('password'), 'a-random-password', { delay: 100 });
 
   // See https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
   await userEvent.click(canvas.getByRole('button'));
@@ -39,7 +36,7 @@ FilledForm.play = async ({ canvasElement }) => {
   // 👇 Assert DOM structure
   await expect(
     canvas.getByText(
-      'Everything is perfect. Your account is ready and we should probably get you started!'
+      'Login successful!'
     )
   ).toBeInTheDocument();
 };
--- src/stories/LoginForm.tsx
+++ src/stories/LoginForm.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
+import { Input, Button, Alert } from 'antd';
+import "antd/dist/antd.css";
 
 const LoginForm: React.FC = () => {
   const [showSucces, setShowSuccess] = useState(false);  
   return (
-    <div style={{
-      border: '1px solid', 
-      display: 'flex', 
-      flexDirection: 'column', 
-      width: '20%'
-      }}>
-      <input type='email' data-testid="email" />
-      <input type='password' data-testid="password" />
-      <button onClick={() => setShowSuccess(true)}>Login</button>
+    <>
+      <div style={{
+        border: '1px solid', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        width: '20%',
+        padding: '4px',
+        marginBottom: '20px',
+        }}>
+          <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '4px' }}>
+            <span>Email</span>
+            <Input type='email' data-testid="email" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
+            <span>Password</span>
+            <Input type='password' data-testid="password" />
+          </div>
+        <Button type='primary' onClick={() => setShowSuccess(true)}>Login</Button>
+      </div>
+
       {showSucces && 
-        <span>Everything is perfect. Your account is ready and we should probably get you started!</span>
+        <Alert style={{ width: '20%' }} showIcon type='success' message="Login successful!" />
       }
-    </div>
+    </>
   )
 };
 
