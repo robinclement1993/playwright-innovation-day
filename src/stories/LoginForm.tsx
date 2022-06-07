c115f25858a8ddfe20a588e9609ba12187e3fe3a
robinclement1993 <41688771+robinclement1993@users.noreply.github.com>
2022-06-07 14:26:45 +0200

    Add LoginForm component for simple interaction test

--- src/stories/LoginForm.tsx
+++ src/stories/LoginForm.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const LoginForm: React.FC = () => (
-  <div style={{
-    border: '1px solid', 
-    display: 'flex', 
-    flexDirection: 'column', 
-    width: '20%'
-    }}>
-    <input type='email' data-testid="email" />
-    <input type='password' data-testid="password" />
-  </div>
-);
+const LoginForm: React.FC = () => {
+  const [showSucces, setShowSuccess] = useState(false);  
+  return (
+    <div style={{
+      border: '1px solid', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      width: '20%'
+      }}>
+      <input type='email' data-testid="email" />
+      <input type='password' data-testid="password" />
+      <button onClick={() => setShowSuccess(true)}>Login</button>
+      {!showSucces && 
+        <span>Everything is perfect. Your account is ready and we should probably get you started!</span>
+      }
+    </div>
+  )
+};
 
 export default LoginForm;
